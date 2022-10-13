4fa943628fc51621b6d5cfcd8c6165034c033ce5
diegui98 <diegui98@live.com.ar>
2022-10-13 19:31:41 -0300

    new catalog, catalog setup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
-  declarations: [ProductCardComponent, CatalogComponent],
+  declarations: [ProductCardComponent, CatalogComponent, CategoryComponent],
   imports: [CommonModule],
-  exports: [ProductCardComponent, CatalogComponent],
+  exports: [ProductCardComponent, CatalogComponent, CategoryComponent],
 })
 export class SharedModule {}
