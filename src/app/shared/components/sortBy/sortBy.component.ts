d61d9662fa0e4b6c6a21c1f228090f069f9f11ca
diegui98 <diegui98@live.com.ar>
2022-10-15 18:09:55 -0300

    orderlist method, sort + erversort method

--- src/app/shared/components/sortBy/sortBy.component.ts
+++ src/app/shared/components/sortBy/sortBy.component.ts
@@ -10,42 +10,10 @@ import { ProductCard } from 'src/app/core/models/productCard';
 })
 export class SortByComponent implements OnInit {
   @Input()
-  productList?: ProductCard[] = [
-    {
-      name: 'Green Shirt',
-      image: [
-        { image: ['noimage.jpg', 'noimage2.png'], color: 'green' },
-        { image: ['noimage.jpg', 'noimage2.png'], color: 'blue' },
-      ],
-      price: 80,
-      sale: false,
-      inStock: true,
-    },
-    {
-      name: 'Blue Pants',
-      image: [{ image: ['noimage.jpg', 'noimage2.png'], color: 'blue' }],
-      price: 100,
-      sale: false,
-      inStock: true,
-    },
-    {
-      name: 'Red Shoes',
-      image: [{ image: ['noimage.jpg', 'noimage2.png'], color: 'Red' }],
-      price: 120,
-      sale: false,
-      inStock: false,
-    },
-    {
-      name: 'Black Hat',
-      image: [{ image: ['noimage.jpg', 'noimage2.png'], color: 'Black' }],
-      price: 40,
-      sale: true,
-      inStock: true,
-    },
-  ];
+  productList?: ProductCard[];
 
   @Output()
-  orderedList!: ProductCard[];
+  orderedList!: ProductCard[] | undefined;
 
   constructor() {}
 
@@ -53,6 +21,44 @@ export class SortByComponent implements OnInit {
 
   orderList(event: any) {
     let value = event.target.value;
-    //finish method
+    if (value == 'A-Z') {
+      this.productList?.sort(this.dynamicSort('name'));
+    }
+    if (value == 'Z-A') {
+      this.productList?.sort(this.dynamicReverseSort('name'));
+    }
+    if (value == 'cheap') {
+      this.productList?.sort(this.dynamicSort('price'));
+    }
+    if (value == 'expensive') {
+      this.productList?.sort(this.dynamicReverseSort('price'));
+    }
+    this.orderedList = this.productList;
+  }
+
+  dynamicSort(property: any) {
+    var sortOrder = 1;
+    if (property[0] === '-') {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a: any, b: any) {
+      var result =
+        a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+      return result * sortOrder;
+    };
+  }
+
+  dynamicReverseSort(property: any) {
+    var sortOrder = 1;
+    if (property[0] === '-') {
+      sortOrder = 1;
+      property = property.substr(1);
+    }
+    return function (a: any, b: any) {
+      var result =
+        a[property] > b[property] ? -1 : a[property] < b[property] ? 1 : 0;
+      return result * sortOrder;
+    };
   }
 }
