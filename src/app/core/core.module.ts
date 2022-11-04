e127e67fb849dfd33951d89fa396df59684ee28a
diegui98 <diegui98@live.com.ar>
2022-11-04 19:54:02 -0300

    new footer comp

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
+  declarations: [NavbarComponent, SidebarComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SidebarComponent],
+  exports: [NavbarComponent, SidebarComponent, FooterComponent],
 })
 export class CoreModule {}
